613f0c4654d28d64bf5a46dd4a3cda5d2891eac9
Tad Preston <tadpreston@gmail.com>
2018-01-10 15:39:28 -0600

    updated the migration

--- db/migrate/20180110195734_create_users.rb
+++ db/migrate/20180110195734_create_users.rb
@@ -1,12 +1,14 @@
 class CreateUsers < ActiveRecord::Migration[5.1]
   def change
     create_table :users do |t|
-      t.string :email
-      t.string :password_digest
+      t.string :email,           null: false, default: ''
+      t.string :password_digest, null: false, default: ''
       t.string :first_name
       t.string :last_name
 
       t.timestamps
     end
+
+    add_index :users, :email, unique: true
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -16,12 +16,13 @@ ActiveRecord::Schema.define(version: 20180110195734) do
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.string "email"
-    t.string "password_digest"
+    t.string "email", default: "", null: false
+    t.string "password_digest", default: "", null: false
     t.string "first_name"
     t.string "last_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
